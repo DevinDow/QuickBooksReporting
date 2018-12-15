de43df74b35296efb44bf62621336e9a3805a911
Devin Dow <DevinDow@gmail.com>
2018-12-15 04:21:51 -0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickBooksReporting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuImportSalesCSV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            ofd.RestoreDirectory = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string line in File.ReadLines(ofd.FileName, Encoding.UTF8))
+                {
+                    string[] fields = line.Split(',');
+
+                }
+            }
+        }
+    }
+}
---
+++ ToDo.md
@@ -0,0 +1,18 @@
+# QuickBooks
+
+## Notes
+
+- QuickBooks exports a .CSV for Year or YTD
+- Rules table provides data to transform by.
+  - How is Rules table entered?  Excel to .CSV?  Interactive?
+- Generate Weekly & Quarterly Reports
+- Use static data from previous years plus new data exported YTD
+
+## ToDo
+
+- Normalize Data
+  - Customer
+  - Item
+  - flag unmatched entries
+    - listbox?
+    - interactive?
