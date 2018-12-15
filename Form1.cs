1a50cc4441eb18b5fcdf46e613f31dbd37102050
Devin Dow <DevinDow@gmail.com>
2018-12-15 06:04:38 -0800

    Track unique Names & Items.

--- Form1.cs
+++ Form1.cs
@@ -34,20 +34,30 @@ namespace QuickBooksReporting
             {
                 Cursor.Current = Cursors.WaitCursor;
 
+                // parse CSV
                 DateTime start = DateTime.Now;
                 stsInfo.Text = string.Format("Reading {0}", ofd.FileName);
                 Sales.ParseCSV(ofd.FileName);
                 stsInfo.Text = string.Format("{0} parsed in {1} seconds", ofd.FileName, DateTime.Now - start);
                 Application.DoEvents(); // update the Status Bar before continuing
 
+                // fill Invoices
                 lblInvoices.Text = string.Format("{0:n0} Invoices", Sales.Invoices.Count);
                 lstInvoices.Items.AddRange(Sales.Invoices.ToArray());
 
+                // fill Credits
                 lblCredits.Text = string.Format("{0:n0} Credits", Sales.Credits.Count);
                 lstCredits.Items.AddRange(Sales.Credits.ToArray());
 
+                // fill Skipped
                 lblSkipped.Text = string.Format("{0:n0} Skipped", Sales.Skipped.Count);
                 lstSkipped.Items.AddRange(Sales.Skipped.ToArray());
+
+                // fill unique Names
+                lstNames.Items.AddRange(Sales.Names.Keys.ToArray());
+
+                // fill unique Items
+                lstItems.Items.AddRange(Sales.Items.Keys.ToArray());
 
                 Cursor.Current = Cursors.Default;
             }
--- Sales.cs
+++ Sales.cs
@@ -10,16 +10,20 @@ namespace QuickBooksReporting
     class Sales
     {
         // Public Fields
-        public string Name;
+        public string Filename;
+
         public List<LineItem> Invoices = new List<LineItem>();
         public List<LineItem> Credits = new List<LineItem>();
         public List<string> Skipped = new List<string>();
+
+        public Dictionary<string, List<LineItem>> Names = new Dictionary<string, List<LineItem>>();
+        public Dictionary<string, List<LineItem>> Items = new Dictionary<string, List<LineItem>>();
 
 
         // Methods
         public void ParseCSV(string filename)
         {
-            Name = filename;
+            Filename = filename;
             foreach (string line in File.ReadLines(filename, Encoding.UTF8))
             {
                 string[] fields = line.Split(',');
@@ -27,16 +31,33 @@ namespace QuickBooksReporting
                 if (lineItem.type == "Invoice")
                 {
                     Invoices.Add(lineItem);
+                    TrackUnique(lineItem);
                 }
                 else if (lineItem.type == "Credit Memo")
                 {
                     Credits.Add(lineItem);
+                    TrackUnique(lineItem);
                 }
                 else
                 {
                     Skipped.Add(line);
                 }
             }
+        }
+
+        private void TrackUnique(LineItem lineItem)
+        {
+            if (!Names.ContainsKey(lineItem.name))
+            {
+                Names.Add(lineItem.name, new List<LineItem>());
+            }
+            Names[lineItem.name].Add(lineItem);
+
+            if (!Items.ContainsKey(lineItem.item))
+            {
+                Items.Add(lineItem.item, new List<LineItem>());
+            }
+            Items[lineItem.item].Add(lineItem);
         }
     }
 }
