05d01095cd10c113c3ca3e884af11809b737920e
Devin Dow <DevinDow@gmail.com>
2019-09-04 14:16:08 -0700

    Customer Summary CSV

--- CSV.cs
+++ CSV.cs
@@ -50,15 +50,18 @@ namespace QuickBooksReporting
 
         private void generateDetailed()
         {
+            // write Header
             string[] columns = Report.MergeColumns(new string[] { "Date", "Customer", "Qty", "Price Each", "Total Sales" }, Items.Columns);
             Writer.WriteLine(string.Join(",", columns));
 
+            // loop LineItems
             foreach (LineItem lineItem in Sales.Invoices)
             {
                 // Filter by Date range
                 if (lineItem.date < From || lineItem.date > To)
                     continue;
 
+                // write LineItem
                 columns = new string[] { lineItem.date.ToShortDateString(), lineItem.CustomerName, lineItem.quantity.ToString(), string.Format("${0}", lineItem.price), string.Format("${0}", lineItem.Subtotal) };
                 if (lineItem.itemMap != null)
                 {
@@ -75,18 +78,19 @@ namespace QuickBooksReporting
 
         private void generateItemSummary()
         {
+            // write Header
             string[] columns = new string[] { "Product", "Qty", "Total Sales" };
             Writer.WriteLine(string.Join(",", columns));
 
-            // Filter & Collect
+            // filter LineItems & collect Items
             SortedDictionary<string, List<LineItem>> itemMap = new SortedDictionary<string, List<LineItem>>();
             foreach (LineItem lineItem in Sales.Invoices)
             {
-                // Filter by Date range
+                // filter by Date range
                 if (lineItem.date < From || lineItem.date > To)
                     continue;
 
-                // Collect LineItems by ItemName
+                // collect LineItems by ItemName
                 if (!itemMap.ContainsKey(lineItem.ItemName))
                 {
                     itemMap.Add(lineItem.ItemName, new List<LineItem>());
@@ -95,6 +99,7 @@ namespace QuickBooksReporting
                 itemMap[lineItem.ItemName].Add(lineItem);
             }
 
+            // loop Items
             foreach (var itemEntry in itemMap)
             {
                 int quantity = 0;
@@ -104,6 +109,8 @@ namespace QuickBooksReporting
                     quantity += lineItem.quantity;
                     subtotal += lineItem.Subtotal;
                 }
+
+                // write Item summary
                 columns = new string[] { itemEntry.Key, quantity.ToString(), string.Format("${0}", subtotal) };
                 Writer.WriteLine(string.Join(",", columns));
             }
@@ -111,6 +118,58 @@ namespace QuickBooksReporting
 
         private void generateCustomerSummary()
         {
+            // write Header
+            string[] columns = new string[] { "Customer", "Product", "Qty", "Total Sales" };
+            Writer.WriteLine(string.Join(",", columns));
+
+            // filter LineItems & collect LineItems by Customer
+            SortedDictionary<string, List<LineItem>> customerMap = new SortedDictionary<string, List<LineItem>>();
+            foreach (LineItem lineItem in Sales.Invoices)
+            {
+                // filter by Date range
+                if (lineItem.date < From || lineItem.date > To)
+                    continue;
+
+                // collect LineItems by CustomerName
+                if (!customerMap.ContainsKey(lineItem.CustomerName))
+                {
+                    customerMap.Add(lineItem.CustomerName, new List<LineItem>());
+                }
+
+                customerMap[lineItem.CustomerName].Add(lineItem);
+            }
+
+            // loop Customers
+            foreach (var customerEntry in customerMap)
+            {
+                // collect LineItems by Items for this Customer
+                SortedDictionary<string, List<LineItem>> itemMap = new SortedDictionary<string, List<LineItem>>();
+                foreach (LineItem lineItem in customerEntry.Value)
+                {
+                    // collect LineItems by ItemName
+                    if (!itemMap.ContainsKey(lineItem.ItemName))
+                    {
+                        itemMap.Add(lineItem.ItemName, new List<LineItem>());
+                    }
+
+                    itemMap[lineItem.ItemName].Add(lineItem);
+                }
+
+                // loop Items for this Customer
+                foreach (var itemEntry in itemMap)
+                {
+                    // loop LineItems for this Item for this Customer
+                    int quantity = 0;
+                    decimal subtotal = 0;
+                    foreach (LineItem lineItem in itemEntry.Value)
+                    {
+                        quantity += lineItem.quantity;
+                        subtotal += lineItem.Subtotal;
+                    }
+                    columns = new string[] { customerEntry.Key, itemEntry.Key, quantity.ToString(), string.Format("${0}", subtotal) };
+                    Writer.WriteLine(string.Join(",", columns));
+                }
+            }
         }
     }
 }
